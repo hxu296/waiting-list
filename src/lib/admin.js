d54e13102e9ca33d636097a40caf0a2910fdf5ac
Huan Xu <hxu296@wisc.edu>
2022-09-04 01:20:39 -0500

    chore: use VITE_VERCEL_URL if available

--- src/lib/admin.js
+++ src/lib/admin.js
@@ -4,8 +4,6 @@ import { createClient } from '@supabase/supabase-js'
 dotenv.config()
 const env = process.env
 
-env.VITE_APP_URL = env.VITE_VERCEL_URL || env.VITE_APP_URL
-
 const supabase = createClient(env.VITE_SUPABASE_URL, env.SUPABASE_SERVICE_ROLE_KEY)
 
 export default supabase
